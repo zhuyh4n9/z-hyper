68b5be016bb2245c552676e5333557ce0686aa9a
zhuyh4n9 <opui123415@gmail.com>
2026-07-17 01:02:46 +0800

    optimize build

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ NR_CPU=1
 HZ=250
 TRACE_ON=1
 
+TOP_DIR=$(pwd)
+
 if [ -d "./build" ]; then
     rm -rf ./build
 fi
@@ -14,5 +16,86 @@ fi
 mkdir -p build
 cd build
 
-cmake .. -DNR_CPU=${NR_CPU} -DHZ=${HZ} -DTRACE_ON=${TRACE_ON} -DCMAKE_BUILD_TYPE=Debug
-make
+# usage: build.sh [-b ninja|make] [-a KEY=VALUE]... [-f CONFIG_FILE]
+BUILD_SYSTEM="ninja"
+declare -A CONFIG=(
+    [NR_CPU]=1
+    [HZ]=250
+    [TRACE_ON]=1
+)
+
+# 解析 -f 指定的配置文件（KEY=VALUE 格式，每行一个，# 开头为注释）
+parse_config_file() {
+    local file="$1"
+    if [ ! -f "$file" ]; then
+        echo "error: config file '$file' not found"
+        exit 1
+    fi
+    while IFS='=' read -r key value; do
+        # 跳过空行和注释
+        [[ -z "$key" || "$key" == \#* ]] && continue
+        # 去掉首尾空白
+        key=$(echo "$key" | xargs)
+        value=$(echo "$value" | xargs)
+        CONFIG["$key"]="$value"
+        echo "  [config] $key=$value"
+    done < "$file"
+}
+
+while getopts "b:a:f:hr:" opt; do
+    case $opt in
+        b)
+            BUILD_SYSTEM="$OPTARG"
+            ;;
+        a)
+            IFS='=' read -r key value <<< "$OPTARG"
+            CONFIG["$key"]="$value"
+            ;;
+        f)
+            echo "reading config from: $OPTARG"
+            parse_config_file "$OPTARG"
+            ;;
+        h)
+            echo "usage: $0 [-b ninja|make] [-a KEY=VALUE]... [-f CONFIG_FILE]"
+            echo "  -b  Choose build system: ninja (default) or make"
+            echo "  -a  Pass configuration items, can be used multiple times, e.g.: -a NR_CPU=4 -a HZ=1000"
+            echo "  -f  Read configuration from a file in KEY=VALUE format"
+            echo ""
+            exit 0
+            ;;
+        r)
+            RUN_MODE="$OPTARG"
+            ;;
+        *)
+            echo "usage: $0 [-b ninja|make] [-a KEY=VALUE]... [-f CONFIG_FILE]"
+            exit 1
+            ;;
+    esac
+done
+
+CMAKE_DEFS=""
+for key in "${!CONFIG[@]}"; do
+    CMAKE_DEFS+=" -D${key}=${CONFIG[$key]}"
+done
+
+echo "build system : $BUILD_SYSTEM"
+echo "config       :${CMAKE_DEFS}"
+
+if [ "$BUILD_SYSTEM" = "ninja" ]; then
+    cmake .. -G Ninja ${CMAKE_DEFS} -DCMAKE_BUILD_TYPE=Debug
+    ninja
+elif [ "$BUILD_SYSTEM" = "make" ]; then
+    cmake .. ${CMAKE_DEFS} -DCMAKE_BUILD_TYPE=Debug
+    make
+else
+    echo "error: unknown build system '$BUILD_SYSTEM', use ninja or make"
+    exit 1
+fi
+
+if [ "$RUN_MODE" = "normal" ]; then
+    echo "running z-hyper..."
+    bash $TOP_DIR/run.sh -r $TOP_DIR/build/z-hyper.elf
+elif [ "$RUN_MODE" = "debug" ]; then
+    echo "running z-hyper in debug mode..."
+    bash $TOP_DIR/run.sh -d $TOP_DIR/build/z-hyper.elf
+fi
--- run.sh
+++ run.sh
@@ -6,16 +6,47 @@ QEMU=qemu-system-aarch64
 GIC_VERSION=3
 MACHINE="virt,gic-version=$GIC_VERSION,virtualization=on"
 NCPU=1
-QEMUOPTS="-cpu $QCPU -machine $MACHINE -smp $NCPU -m 256M -nographic\
-          -kernel build/z-hyper.elf"
+RUN_MODE="normal"
 
-if [ "$1" == "debug" ]; then
-    echo "waiting for gdb..."
+# run.sh [-r|-d|-k] [-c NCPU] [elf_file]
+while getopts "rdkc:h" opt; do
+    case $opt in
+        r) RUN_MODE="normal" ;;
+        d) RUN_MODE="debug" ;;
+        k) RUN_MODE="kill" ;;
+        c) NCPU="$OPTARG" ;;
+        h)
+            echo "usage: $0 [-r|-d|-k] [-c NCPU] [elf_file]"
+            echo "  -r  run normally"
+            echo "  -d  run in debug mode (gdb waiting for connection on localhost:7788)"
+            echo "  -k  kill the QEMU process"
+            echo "  -c  number of CPUs (default: 1)"
+            echo "  elf_file  ELF file to load (default: build/z-hyper.elf)"
+            exit 0
+            ;;
+        *)
+            echo "usage: $0 [-r|-d|-k] [-c NCPU] [elf_file]"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+ELF_FILE="${1:-build/z-hyper.elf}"
+
+echo "RUN_MODE=$RUN_MODE, ELF_FILE=$ELF_FILE, NCPU=$NCPU"
+
+QEMUOPTS="-cpu $QCPU -machine $MACHINE -smp $NCPU -m 256M -nographic \
+          -kernel $ELF_FILE"
+
+if [ "$RUN_MODE" = "kill" ]; then
+    echo "killing qemu..."
+    pkill -f "$QEMU"
+    exit 0
+elif [ "$RUN_MODE" = "debug" ]; then
+    echo "gdb waiting for connection on localhost:7788"
     $QEMU $QEMUOPTS -S -gdb tcp::7788
-elif [ "$1" == "kill" ]; then
-    echo "kill all qemu instances ..."
-    kill -9 `pidof qemu-system-aarch64`
+    exit 0
 else
-    echo "run mode ..."
     $QEMU $QEMUOPTS
 fi
